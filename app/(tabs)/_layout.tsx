e6a65311b379e6905e7cdc254a01391eec07d92c
elian-meico0810 <efince@meico.com.co>
2025-11-22 12:09:22 -0500

    files config

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,6 +13,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+         tabBarStyle: { display: "none" },
       }}>
       
     </Tabs>

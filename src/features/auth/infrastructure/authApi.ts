0e78ad6b8243bba7184a95c082ee57db34c48787
elian-meico0810 <efince@meico.com.co>
2026-02-11 09:36:49 -0500

    add link the production

--- src/features/auth/infrastructure/authApi.ts
+++ src/features/auth/infrastructure/authApi.ts
@@ -4,10 +4,12 @@ export const authApi = axios.create({
   //baseURL: "http://127.0.0.1:8000/api/",
   // baseURL: "http://40.124.182.81:8080/api/",
   // baseURL: "http://192.168.72.30:8080/api/",
-  baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
+  // baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
+  baseURL:"https://portal.meico.com.co/meitruck/back/"
 
 });
 
 export const authDevApi = axios.create({
-  baseURL: "https://qa-portal.meico.co/pagos/back/api/"
+  // baseURL: "https://qa-portal.meico.co/pagos/back/api/"
+   baseURL: "https://portal.meico.com.co/pagos/back/api/"
 });

cfd6363aef7d374ca615befc5abebc5a141c3154
elian-meico0810 <efince@meico.com.co>
2026-03-27 20:37:07 -0500

    Add base prod

--- src/features/auth/infrastructure/authApi.ts
+++ src/features/auth/infrastructure/authApi.ts
@@ -5,8 +5,8 @@ export const authApi = axios.create({
   // baseURL: "http://40.124.182.81:8080/api/",
   // baseURL: "http://192.168.72.30:8080/api/",
   // baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
-  // baseURL:"https://portal.meico.com.co/meitruck/back/api/"
-  baseURL: "https://test-meico-dphgc2e5fefha5f7.centralus-01.azurewebsites.net/back/api/"
+  baseURL:"https://portal.meico.com.co/meitruck/back/api/"
+  // baseURL: "https://test-meico-dphgc2e5fefha5f7.centralus-01.azurewebsites.net/back/api/"
   // baseURL: "http://192.168.72.144:8000/api/", 
 
 });

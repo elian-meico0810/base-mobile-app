650e92f9a4939644ffd1fa9485423a8d73c5e02c
elian-meico0810 <efince@meico.com.co>
2026-04-28 18:51:29 -0500

    fix: update API base URLs in authApi and apiRoutes for production environment

--- src/features/auth/infrastructure/authApi.ts
+++ src/features/auth/infrastructure/authApi.ts
@@ -9,12 +9,13 @@ export const authApi = axios.create({
   // baseURL: "http://192.168.72.47:8000/api/",
   // baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
   // baseURL: "http://192.168.72.144:8000/api/",
-  baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
-  // baseURL:"https://portal.meico.com.co/meitruck/back/api/"
+  // baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
+  baseURL:"https://portal.meico.com.co/meitruck/back/api/"
 
 });
 
 export const authDevApi = axios.create({
-  baseURL: "https://qa-portal.meico.co/pagos/back/api/",
-  //  baseURL: "https://portal.meico.com.co/pagos/back/api/"
+  // baseURL: "https://qa-portal.meico.co/pagos/back/api/",
+   baseURL: "https://portal.meico.com.co/pagos/back/api/"
+  
 });

e580e4e187d9a99a092611d41a13522827340f8a
elian-meico0810 <efince@meico.com.co>
2026-06-22 09:26:04 -0500

    fix: update baseURL in authApi and restore validation for WhatsApp number in InfoInvoiceForm

--- src/features/auth/infrastructure/authApi.ts
+++ src/features/auth/infrastructure/authApi.ts
@@ -8,7 +8,7 @@ export const authApi = axios.create({
   // baseURL: "http://40.124.182.81:8080/api/",
   // baseURL: "http://192.168.72.33:8000/api/",
   // baseURL: "https://qa-portal.meico.co/meicotrack/back/api/"
-  baseURL: "http://192.168.72.29:8000/api/",
+  baseURL: "http://192.168.72.94:8000/api/",
   // baseURL: "https://qa-portal.meico.co/meitruck/back/api/"
   // baseURL:"https://portal.meico.com.co/meitruck/back/api/"
 
